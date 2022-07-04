e4bee2024300481d779e42800aeb3b6f446c1a42
Samet OZTURK <sametozturk.info@gmail.com>
2022-07-04 17:22:49 +0300

    Navbar interaktif hale getirildi

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 let lightbulb = document.getElementById("bulb");
 let siteStatus = 0;
+let closeBar = document.getElementById("closeBar");
+let openBar = document.getElementById("openBar");
+let navbar = document.getElementById("navbar");
+let body = document.querySelector("body");
 lightbulb.addEventListener("click",function(){
     if(siteStatus == 0){
         lightbulb.classList.add("btn-on");
@@ -8,4 +12,14 @@ lightbulb.addEventListener("click",function(){
         lightbulb.classList.remove("btn-on");
         siteStatus = 0;
     }
+});
+
+closeBar.addEventListener("click",function(){
+    navbar.style.transform = "translateX("+(100)+"%)";
+    body.classList.remove("max-height");
+});
+
+openBar.addEventListener("click",function(){
+    navbar.style.transform = "translateX("+(0)+"%)";
+    body.classList.add("max-height");
 });
