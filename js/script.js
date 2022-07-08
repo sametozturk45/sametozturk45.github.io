aa8e223367530c212bf7f0482ee1bdb438a161a6
Samet OZTURK <sametozturk.info@gmail.com>
2022-07-08 12:27:52 +0300

    Genel değişiklik ve eklemeler yapıldı

--- js/script.js
+++ js/script.js
@@ -2,7 +2,10 @@ let lightbulb = document.getElementById("bulb");
 let siteStatus = 0;
 let closeBar = document.getElementById("closeBar");
 let openBar = document.getElementById("openBar");
+let openAbout = document.getElementById("openAbout");
 let navbar = document.getElementById("navbar");
+let aboutMe = document.getElementById("aboutMeContent");
+let navbarContent = document.getElementById("navbarContent");
 let body = document.querySelector("body");
 lightbulb.addEventListener("click",function(){
     if(siteStatus == 0){
@@ -17,9 +20,19 @@ lightbulb.addEventListener("click",function(){
 closeBar.addEventListener("click",function(){
     navbar.style.transform = "translateX("+(100)+"%)";
     body.classList.remove("max-height");
+    setTimeout(()=>{navbarContent.classList.remove("d-none");
+    aboutMe.classList.remove("d-none");},500);
 });
 
 openBar.addEventListener("click",function(){
+    aboutMe.classList.add("d-none");
+    navbar.style.transform = "translateX("+(0)+"%)";
+    body.classList.add("max-height");
+    window.scrollTo({top:0,behavior:'smooth'});
+});
+
+openAbout.addEventListener("click",function(){
+    navbarContent.classList.add("d-none");
     navbar.style.transform = "translateX("+(0)+"%)";
     body.classList.add("max-height");
     window.scrollTo({top:0,behavior:'smooth'});
